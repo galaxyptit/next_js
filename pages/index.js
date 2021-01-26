8d7dbe9dea219f164924557925fe91fb66e1a5ef
galaxyptit <44774814+galaxyptit@users.noreply.github.com>
2021-01-26 13:38:12 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          alo 123456789
         </h1>
 
         <p className={styles.description}>
